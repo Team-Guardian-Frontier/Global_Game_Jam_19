b4601c1cb4e5e155f27a793c3bd587df67764f73
LAPTOP-IA658AG2\It's Dillon Zhong <pineapplegamedesign@gmail.com>
2019-01-27 12:11:23 -0500

    Spacebar Food

---
+++ TGF_GGJ_19/Assets/Scripts/DLScripts/DLScript_C1S1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DLScript_C1S1 : DLScript
+{
+
+    void Start()
+    {
+        StartCoroutine(LateStart(2));
+    }
+
+    void Update()
+    {
+    }
+
+
+    //COROUTINES
+    IEnumerator LateStart(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        //Your Function You Want to Call
+        myDialogue.TriggerDialogue(0);
+
+        /*
+        int[] indices = { 0, 1, 3 };
+        myDialogue.TriggerDialogue(indices);
+        */
+    }
+    }
